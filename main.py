e695c4c75f87a3155288a67068a3fa8df51f11f1
Shukrullo Turgunov <vodiylik@users.noreply.github.com>
2021-01-19 22:57:30 +0500

    a lot of changes

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
+import logging
 from googletrans import Translator
 import telebot
 
 translator = Translator()
 TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(token=TOKEN)
+log = logging.getLogger("pytanslatebot")
 
-hello_message = (
-    "Salom,\nhozirda bot test tartibida ishlamoqda. \nTarjimon  botiga xush kelibsiz! "
-)
+hello_message = "Salom,\nHozirda bot test tartibida ishlamoqda. \nTarjimon  botiga xush kelibsiz! \n"
 msg = "Iltimos ingliz yoki o'zbek tilidagi matnni yozib yuboring."
 
 # handle /start command
@@ -19,15 +19,24 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=["text"])
 def func(message):
-    detected_language = translator.detect(message)
-
-    if detected_language in ["en"]:
-        translated_msg = translator.translate(message, dest="uz").text
-        bot.reply_to(message, translated_msg)
-    elif detected_language in ["uz"]:
-        translated_msg = translator.translate(message, dest="en").text
-        bot.reply_to(message, translated_msg)
-    else:
+    text = str(message.text)
+    try:
+        log.info("Detecting language of text: ", text)
+        detected_language = translator.detect(text).lang
+        log.info("Detected language: ", detected_language)
+        if detected_language in ["en"]:
+            log.info("Translating '", text, "' to english...")
+            msg = translator.translate(text, dest="uz").text
+        elif detected_language in ["uz"]:
+            log.info("Translating '", text, "' to uzbek...")
+            msg = translator.translate(text, dest="en").text
+        else:
+            msg = "Tilni aniqlab bo'lmadi, qaytadan urining!"
+        bot.reply_to(message, msg)
+        log.info("Message translated. Translated message: ", msg)
+    except Exception as e:
+        msg = "Xatolik yuz berdi birozdan so'ng urinib ko'ring!"
+        log.error("Xatolik yuz berdi: ", e)
         bot.reply_to(message, msg)
 
 
